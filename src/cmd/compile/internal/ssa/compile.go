7e93b4569d681e42a16aceb5d2fcf9f3a9a8cbc1
Momchil Velikov <momchil.velikov@gmail.com>
2016-10-22 17:59:58 +0300

    Move the SCCP pass after opt

--- src/cmd/compile/internal/ssa/compile.go
+++ src/cmd/compile/internal/ssa/compile.go
@@ -247,8 +247,8 @@ var passes = [...]pass{
 	{name: "early deadcode", fn: deadcode}, // remove generated dead code to avoid doing pointless work during opt
 	{name: "short circuit", fn: shortcircuit},
 	{name: "decompose user", fn: decomposeUser, required: true},
+	{name: "opt", fn: opt, required: true}, // TODO: split required rules and optimizing rules
 	{name: "sccp", fn: sccp},
-	{name: "opt", fn: opt, required: true},               // TODO: split required rules and optimizing rules
 	{name: "zero arg cse", fn: zcse, required: true},     // required to merge OpSB values
 	{name: "opt deadcode", fn: deadcode, required: true}, // remove any blocks orphaned during opt
 	{name: "generic cse", fn: cse},
